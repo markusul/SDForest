3a339001ce07e6cd97be72b95650e434c4a77fc5
Markus Ulmer <maulmer@student.ethz.ch>
2024-03-01 15:43:18 +0100

    .

--- simulation_study/perf_q.r
+++ simulation_study/perf_q.r
@@ -5,15 +5,15 @@ p <- 500
 
 n_test <- 500
 
-N_rep <- 10
+N_rep <- 2
 
-q_seq <- seq(0, 100, 20)
-#q_seq <- c(0, 100)
+seq <- seq(0, 100, 20)
+seq <- c(0, 100)
 
 
 print('start')
 start <- Sys.time()
-perf_q <- lapply(1:N_rep, function(i) lapply(q_seq, function(q) performance_measure(n, p, q, n_test, eff = NULL)))
-save(perf_q, q_seq, file = gsub('[ :]', '_', paste("simulation_study/results/perf_q/", date(), '.RData', sep='')))
+perf <- lapply(1:N_rep, function(i) lapply(seq, function(q) performance_measure(n, p, q, n_test, eff = NULL)))
+save(perf, seq, file = gsub('[ :]', '_', paste("simulation_study/results/perf_q/", date(), '.RData', sep='')))
 print('q done')
 print(Sys.time() - start)
